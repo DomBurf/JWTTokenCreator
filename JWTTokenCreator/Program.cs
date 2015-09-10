0ba874defc7d785130294831b3726f6240aa3999
Dominic Burford <dominic@dominicburford.co.uk>
2015-09-10 14:31:13 +0100

    Updated

--- JWTTokenCreator/Program.cs
+++ JWTTokenCreator/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-<<<<<<< HEAD
 using Newtonsoft.Json.Linq;
-=======
->>>>>>> c5bdfc677d68371661aeb10b6a9efce92ca2e1ef
 
 namespace JWTTokenCreator
 {
     /// <summary>
-<<<<<<< HEAD
     /// Simple JWT token creator for testing and debugging JWT tokens. Can encode and decode JWT tokens.
     /// Can be invoked from the commandline as follows:
     /// 
@@ -22,36 +18,22 @@ namespace JWTTokenCreator
     /// JWTTokenCreator.exe 2 "jsontoken" "sharedkey"
     /// If invoked without arguments then the JSON token and sharedkey can be entered on the commandline manually.
     /// The token that is decoded is copied to the clipboard.
-=======
-    /// Simple JWT token creator for debugging and testing purposes.
-    /// Can be invoked from the commandline as follows:
-    /// JWTTokenCreator.exe "username" "sharedkey"
-    /// If invoked without arguments then the username and sharedkey can be entered on the commandline manually.
-    /// The token that is created is copied to the clipboard.
->>>>>>> c5bdfc677d68371661aeb10b6a9efce92ca2e1ef
     /// </summary>
     class Program
     {
         [STAThread]
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             string username;
             string sharedkey;
             string jsonWebToken;
             int option;
-=======
-            bool automaticInput = false;
-            string username;
-            string sharedkey;
->>>>>>> c5bdfc677d68371661aeb10b6a9efce92ca2e1ef
 
             Console.Clear();
             Console.WriteLine("JWT Token Creator:");
 
             if (args != null && args.Length > 0)
             {
-<<<<<<< HEAD
                 option = Convert.ToInt32(args[0]);
                 if (option == 1)
                 {
@@ -106,31 +88,6 @@ namespace JWTTokenCreator
                     string jwtToken = DecodeAuthenticationToken(sharedkey, jsonWebToken);
                     Clipboard.SetText(jwtToken);
                 }
-=======
-                username = args[0];
-                sharedkey = args[1];
-                automaticInput = true;
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("Please enter username");
-                username = Console.ReadLine();
-                Console.WriteLine("Please enter shared key");
-                sharedkey = Console.ReadLine();
-            }
-            
-            string jwtToken = CreateAuthenticationToken(username, sharedkey);
-
-            Clipboard.SetText(jwtToken);
-            
-            if (!automaticInput)
-            {
-                Console.WriteLine("JWT token has been copied to the Windows clipboard");
-                Console.WriteLine();
-                Console.WriteLine("JWT token: {0}", jwtToken);
-
->>>>>>> c5bdfc677d68371661aeb10b6a9efce92ca2e1ef
                 Console.WriteLine();
                 Console.WriteLine("Press any key to exit");
                 Console.ReadKey();
@@ -153,7 +110,6 @@ namespace JWTTokenCreator
             string token = JWT.JsonWebToken.Encode(payload, sharedkey, JWT.JwtHashAlgorithm.HS256);
             return token;
         }
-<<<<<<< HEAD
 
         private static string DecodeAuthenticationToken(string sharedkey, string jsonWebToken)
         {
@@ -165,7 +121,5 @@ namespace JWTTokenCreator
             //Guid jwtId = (Guid)root.jti;
             return root.ToString();
         }
-=======
->>>>>>> c5bdfc677d68371661aeb10b6a9efce92ca2e1ef
     }
 }
